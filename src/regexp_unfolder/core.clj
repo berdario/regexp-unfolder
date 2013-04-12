9291e3f125b8272b92448379ca48431622b95e6e
Dario Bertini <berdario@gmail.com>
2013-04-12 22:30:06 +0200

    Inserted state machine recognizer based on core.logic, adapted from Peteris Erins's snippet

--- src/regexp_unfolder/core.clj
+++ src/regexp_unfolder/core.clj
@@ -1,5 +1,6 @@
 (ns regexp-unfolder.core
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta])
+  (:require [clojure.core.logic :as l]))
 
 (def regexp (insta/parser 
              "re = union | simple-re
@@ -45,4 +46,41 @@
 
 (def fns {:re handle-first, :union handle-first, :simple-re handle-first, :concat handle-first, :base-re handle-first, :star star, :plus plus, :elementary-re handle-first, :any any-char, :group handle-first, :set handle-first, :positive-set handle-set, :negative-set handle-negset, :set-items items, :set-item handle-first, :range handle-range, :char handle-char})
 
+ 
+;; Encoding a regexp finite state machine in core.logic, adapted from a snippet made by Peteris Erins
+;; the state space is #{'ok 'fail}, 'ok is the accepting state, the alphabet is made of all the printable characters
+;; most of the logic thus deals with the starting state and the transitions
 
+(declare transitions)
+ 
+(defn transitiono [state trans new transitions]
+  (l/conde
+   [(l/fresh [f] 
+             (l/firsto transitions f)
+             (l/== [state trans new] f))]
+   [(l/fresh [r]
+             (l/resto transitions r)
+             (transitiono state trans new r))])
+  )
+ 
+(defn recognizeo
+  ([input]
+     (l/fresh [q0]
+            (recognize q0 input)))
+  ([q input]
+     (l/matche [input] ; start pattern matching on the input
+        (['("")]
+           (l/== q 'ok)) ; accept the empty string if we are in an accepting state
+        ([[i . nput]]
+           (l/fresh [qto]
+                  (transitiono q i qto transitions) ; assert it must be what we transition to qto from q with input symbol i
+                  (recognize qto nput)))))) ; recognize the remainder
+ 
+
+(defn unfold [regex] 
+  (def transitions
+    [['ok \a 'ok]
+     ['ok \b 'fail]
+     ['fail \a 'fail]
+     ['fail \b 'fail]] )
+  (map (partial apply str) (l/run* [q] (recognizeo q))))
